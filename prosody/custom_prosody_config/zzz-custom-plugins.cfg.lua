f46ad207f38e699a18d0243df93c4f483ed77701
AmirMehr <amir.mehrnam@yahoo.com>
2025-11-15 14:27:18 +0330

    to reterun back to what was working1

--- prosody/custom_prosody_config/zzz-custom-plugins.cfg.lua
+++
@@ -1,9 +0,0 @@
--- This file enables our custom token plugins
--- This path '/prosody-plugins-custom' matches the mount point in docker-compose.yml
-plugin_paths = { "/prosody-plugins-custom" }
-
--- Enable the plugins for the conference component
-Component "conference.{{ .Env.XMPP_DOMAIN }}" "muc"
-    modules_enabled = {
-        "token_affiliation"; -- Reads 'affiliation' from JWT
-    }
